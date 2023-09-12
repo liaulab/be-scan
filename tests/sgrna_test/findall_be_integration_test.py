cde146f7c015a942874e2057232bf6a6d314c1fc
Calvin XiaoYang Hu <calvinxyh@Calvins-MacBook-Pro.local>
2023-09-11 23:22:31 -0400

    added some notes

--- tests/sgrna_test/findall_be_integration_test.py
+++ tests/sgrna_test/findall_be_integration_test.py
@@ -34,7 +34,9 @@ def test_findall_BE_opt_window_integration_pos():
 def test_findall_BE_opt_guide_length_integration_pos():
     os.system("python3 ../bigscam/sgrna/findall_BE.py test_data/230408_AR_Input.fasta CBE SpG test_data/P10275.fasta --guide_length 26") 
     os.system("rm _SpG_CBE_library.csv")
+    # os.system("python3 ../bigscam/sgrna/findall_BE.py test_data/230408_AR_Input.fasta CBE SpG test_data/P10275.fasta --guide_length 25") 
+    # os.system("rm _SpG_CBE_library.csv")
 
 
-# negative control tests
-# guide length can't be 25 but can be 26, error only shows up when it is checking amino acid mutations
+# add negative control tests, but don't know how since os.system commands aren't caught when it errors other than with reading the results from "pytest -s"
+# guide length can't be 25 but can be 26, error only shows up when it is checking amino acid mutations so wasn't caught by unit tests
