a970d6eb07dc81f9eca94b42627e06a8592c2c11
Simon P Shen <sshen8@gmail.com>
2023-10-01 02:21:02 -0400

    test guide matching

--- tests/test_count_reads.py
+++ tests/test_count_reads.py
@@ -2,6 +2,7 @@ from bigscam.analysis import count_reads
 import pandas as pd
 import os
 import pytest
+import uuid
 
 def test_count_reads():
     with pytest.warns(UserWarning, match="missing column"):
@@ -17,3 +18,43 @@ def test_count_reads():
     os.remove("tests/test_data/counts.csv")
     os.remove("tests/test_data/counts_np.csv")
     os.remove("tests/test_data/counts_stats.txt")
+
+@pytest.mark.parametrize("query,ref,match", [
+    ("G" + "A" * 20, "A" * 20, True),
+    ("G" + "T" * 20, "A" * 20, False)])
+def test_matching(query, ref, match):
+    try:
+        # create fake files
+        test_id = str(uuid.uuid4())
+        fname_query = os.path.join("tests/test_data", test_id + ".fastq")
+        read = "TTGTGGAAAGGACGAAACACC" + query + "GTTTTAGAGCTAGAAATAGCAAGTTAAAATAAGGCTAGTCCGTTATCAACTTGAAAAAGTGGCACCGAGTCGGTGCTTTTTTAAGCTTGGCGTAACTAGATCTTGAGAC"
+        with open(fname_query, "wt") as fh:
+            fh.writelines([
+                "@read1\n",
+                read + "\n",
+                "+\n",
+                "F" * len(read) + "\n"])
+
+        fname_ref = os.path.join("tests/test_data", test_id + ".csv")
+        with open(fname_ref, "wt") as fh:
+            fh.writelines(["sgRNA_seq\n", ref + "\n"])
+        
+        # run count_reads
+        with pytest.warns(UserWarning, match="missing column"):
+            count_reads(fname_query, fname_ref,
+                        out_counts=f"tests/test_data/{test_id}counts.csv",
+                        out_np=f"tests/test_data/{test_id}counts_np.csv",
+                        out_stats=f"tests/test_data/{test_id}counts_stats.txt")
+        # check whether it's counted as a match
+        with open(f"tests/test_data/{test_id}counts.csv", "rt") as fh:
+            line = fh.readline().rstrip()
+        assert line == f"{ref},{int(match)}"
+    finally:
+        # clean up
+        for fname in [fname_query,
+                      fname_ref,
+                      f"tests/test_data/{test_id}counts.csv",
+                      f"tests/test_data/{test_id}counts_np.csv",
+                      f"tests/test_data/{test_id}counts_stats.txt"]:
+            if os.path.exists(fname):
+                os.remove(fname)
