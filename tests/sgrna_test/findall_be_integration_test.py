3986262573da1326890e9740163fda932c75c379
Calvin XiaoYang Hu <calvinxyh@dhcp-10-250-163-58.harvard.edu>
2023-11-15 18:01:36 -0500

    commented out _library_.py, fixed tests

--- tests/sgrna_test/findall_be_integration_test.py
+++ tests/sgrna_test/findall_be_integration_test.py
@@ -26,27 +26,30 @@ def test_findall_BE_basic_integration_pos():
     os.system("rm _SpG_CtoT_library.csv")
     out = subprocess.run("python3 -m bigscam findall_be -g tests/test_data/230408_AR_Input.fasta -e1 A -e2 G -c SpG -p tests/test_data/P10275.fasta", shell=True, capture_output=True)
     assert out.returncode == 0
-    assert filecmp.cmp("_SpG_ABE_library.csv", "tests/ref_data/AR_ABESpG_library.csv")
+    assert filecmp.cmp("_SpG_AtoG_library.csv", "tests/ref_data/AR_ABESpG_library.csv")
     os.system("rm _SpG_AtoG_library.csv")
 
     out = subprocess.run("python3 -m bigscam findall_be -g tests/test_data/230408_AR_Input.fasta -e1 C -e2 T -c SpRY -p tests/test_data/P10275.fasta", shell=True, capture_output=True)
     assert out.returncode == 0
-    assert filecmp.cmp("_SpRY_CBE_library.csv", "tests/ref_data/AR_CBESpRY_library.csv")
+    assert filecmp.cmp("_SpRY_CtoT_library.csv", "tests/ref_data/AR_CBESpRY_library.csv")
     os.system("rm _SpRY_CtoT_library.csv")
     out = subprocess.run("python3 -m bigscam findall_be -g tests/test_data/230408_AR_Input.fasta -e1 A -e2 G -c SpRY -p tests/test_data/P10275.fasta", shell=True, capture_output=True)
     assert out.returncode == 0
-    assert filecmp.cmp("_SpRY_ABE_library.csv", "tests/ref_data/AR_ABESpRY_library.csv")
+    assert filecmp.cmp("_SpRY_AtoG_library.csv", "tests/ref_data/AR_ABESpRY_library.csv")
     os.system("rm _SpRY_AtoG_library.csv")
 
 def test_findall_BE_opt_output_integration_pos():
     out = subprocess.run("python3 -m bigscam findall_be -g tests/test_data/230408_AR_Input.fasta -e1 A -e2 G -c SpRY -p tests/test_data/P10275.fasta --output_prefix 230907_AR --output_dir ../", shell=True, capture_output=True)
     assert out.returncode == 0
+    assert filecmp.cmp("../230907_AR_SpRY_AtoG_library.csv", "tests/ref_data/AR_ABESpRY_library.csv")
     os.system("rm ../230907_AR_SpRY_AtoG_library.csv")
     out = subprocess.run("python3 -m bigscam findall_be -g tests/test_data/230408_AR_Input.fasta -e1 C -e2 T -c SpRY -p tests/test_data/P10275.fasta --output_prefix 230907_AR --output_dir ../ --output_type tsv", shell=True, capture_output=True)
     assert out.returncode == 0
+    assert filecmp.cmp("../230907_AR_SpRY_CtoT_library.tsv", "tests/ref_data/230907_AR_SpRY_CtoT_library.tsv")
     os.system("rm ../230907_AR_SpRY_CtoT_library.tsv")
     out = subprocess.run("python3 -m bigscam findall_be -g tests/test_data/230408_AR_Input.fasta -e1 A -e2 G -c Sp -p tests/test_data/P10275.fasta --output_prefix 230907_AR", shell=True, capture_output=True)
     assert out.returncode == 0
+    assert filecmp.cmp("230907_AR_Sp_AtoG_library.csv", "tests/ref_data/AR_ABESp_library.csv")
     os.system("rm 230907_AR_Sp_AtoG_library.csv")
 
 def test_findall_BE_opt_pam_integration_pos():
@@ -61,14 +64,4 @@ def test_findall_BE_opt_window_integration_pos():
     assert filecmp.cmp("_SpG_CtoT_library.csv", "tests/ref_data/AR_CBESpG_window37_library.csv")
     os.system("rm _SpG_CtoT_library.csv")
 
-def test_findall_BE_opt_guide_length_integration_pos():
-    out = subprocess.run("python3 -m bigscam findall_be -g tests/test_data/230408_AR_Input.fasta -e1 C -e2 T -c SpG -p tests/test_data/P10275.fasta --guide_length 26", shell=True, capture_output=True) 
-    assert out.returncode == 0
-    # assert filecmp.cmp("_SpG_CBE_library.csv", "tests/ref_data/AR_CBESpG_len26_library.csv")
-    os.system("rm _SpG_CtoT_library.csv")
-    # out = subprocess.run("python3 -m bigscam findall_be -g tests/test_data/230408_AR_Input.fasta -e1 C -e2 T -c SpG -p tests/test_data/P10275.fasta --guide_length 25", shell=True, capture_output=True) 
-    # assert out.returncode == 0
-    # os.system("rm _SpG_CBE_library.csv")
-
-# change data format of output to have edits like T878A etc
-# add negative control tests, but don't know how since os.system commands aren't caught when it errors other than with reading the results from "pytest -s"
+### add negative control tests, but don't know how since os.system commands aren't caught when it errors other than with reading the results from "pytest -s"
