9e4d49a9b444fe2d50476bce125a52607452a5c6
Calvin XiaoYang Hu <calvinxyh@dhcp-10-250-163-58.harvard.edu>
2023-12-07 14:42:42 -0500

    added check_guides test, added files from output of both gen_guide and check_guides into test_data

--- tests/sgrna_test/check_guides_test.py
+++ tests/sgrna_test/check_guides_test.py
@@ -8,3 +8,11 @@ Date: 230911
 import pytest
 from be_scan.sgrna.check_guides import check_guides
 
+def test_generate_BE_guides_basic_pos():
+    check_guides(guides_file='tests/test_data/sgrna_data/ARSpGCBE_guides.csv', 
+                 genome_file='../reference_genomes/GCF_000001405.26/ncbi_dataset/data/GCF_000001405.26/GCF_000001405.26_GRCh38_genomic.fna', 
+                 )
+    check_guides(guides_file='tests/test_data/sgrna_data/ARSpRYABE_guides.csv', 
+                 genome_file='../reference_genomes/GCF_000001405.26/ncbi_dataset/data/GCF_000001405.26/GCF_000001405.26_GRCh38_genomic.fna', 
+                 )
+    
