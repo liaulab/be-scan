ade300252ae23c87591f861c2004854466baec93
Calvin XiaoYang Hu <calvinxyh@Calvins-MacBook-Pro.local>
2024-03-19 00:19:32 -0400

    added sgrna enrichment function to __init__

--- be_scan/plot/__init__.py
+++ be_scan/plot/__init__.py
@@ -2,6 +2,7 @@ from be_scan.plot.scatterplot import scatterplot
 from be_scan.plot.correlation_jointplot import corr_jointplot
 from be_scan.plot.correlation_heatmap import corr_heatmap
 from be_scan.plot.boxplot import boxplot
+from be_scan.plot.sgrna_enrichment import sgrna_enrichment
 
 # try: 
 #     from .scatterplot import scatterplot
