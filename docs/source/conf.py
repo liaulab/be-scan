46e5a5a8967ec769eda79001a6223331217f4648
Calvin XiaoYang Hu <calvinxyh@dhcp-10-250-118-35.harvard.edu>
2025-01-30 19:26:48 -0500

    conf file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ import sys
 MOCK_MODULES = ['statsmodels', 'statsmodels.stats.multitest', 
                 'statsmodels.nonparametric.smoothers_lowess', 
                 'statsmodels.sandbox.regression.predstd', 
+                'plotly.graph_objects', 'plotly.express', 'plotly.subplots', 
                 ]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
