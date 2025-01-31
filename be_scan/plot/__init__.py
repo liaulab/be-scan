8aa757063e138e559fa55e43eadc3ea14e231cf5
Calvin XiaoYang Hu <calvinxyh@Calvins-MacBook-Pro.local>
2025-01-31 16:03:36 -0500

    loess range init save

--- be_scan/plot/__init__.py
+++ be_scan/plot/__init__.py
@@ -3,5 +3,5 @@ from be_scan.plot.correlation_jointplot import corr_jointplot, interactive_corr_
 from be_scan.plot.correlation_heatmap import corr_heatmap, interactive_corr_heatmap
 from be_scan.plot.boxplot import boxplot, interactive_boxplot
 from be_scan.plot.sgrna_enrichment import sgrna_enrichment
-from be_scan.plot.loess_smoothing import loess_smoothing, loess_range
+from be_scan.plot.loess_smoothing import loess_smoothing, loess_rangegit
 from be_scan.plot.clustering import pwes_clustering
