ce9cfa64934164db6aea8ae1f86b45b86ce39a6e
Calvin XiaoYang Hu <calvinxyh@dhcp-10-250-98-212.harvard.edu>
2025-08-26 16:04:06 -0400

    untrack tests/

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import pytest
-
-@pytest.fixture(autouse=True)
-def matplotlib_hide_plots(monkeypatch):
-    # prevent plots from opening during test
-    monkeypatch.setattr(plt, "show", lambda: None)
